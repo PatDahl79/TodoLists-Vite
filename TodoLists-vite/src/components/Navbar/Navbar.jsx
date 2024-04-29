f6ab1f20dcb82ce68460c32558a4471319d46d76
PatDahl79 <pattaravarat.dahl@chasacademy.se>
2024-04-29 12:26:25 +0200

    1
    1

---
+++ TodoLists-vite/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo'
+
+
+const Navbar = ()  => {
+    return ( 
+        <div className='navbar'>
+            <img src="" alt="" className="src" />
+
+        </div>
+    )
+}
+
+export default Navbar
